07d25697c331fe3890518d40efb00be03a4fc7ca
KEI <>
2020-08-29 22:18:06 +0900

    ・new/create画面の実装 ・Boardのバリデーション追加
    ＜次回課題＞
    ・Activestorageでプロフ画像変更機能の追加
    ・dropdownボタンが押せない問題

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end
 end
