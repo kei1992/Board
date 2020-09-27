c52e712fefe7ffd3c68b7fe8bc8aa1a8eb02b3f0
KEI <>
2020-09-27 20:25:24 +0900

    ブックマークのajax化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,21 @@
 class BookmarksController < ApplicationController
 
+	def show
+		board = Board.find(params[:board_id])
+		bookmark_status = current_user.has_bookmarked?(board)
+		render json: {hasBookmarked: bookmark_status}
+	end
+
 	def create
 		board = Board.find(params[:board_id])
 		board.bookmarks.create!(user_id: current_user.id)
-		redirect_to boards_path(board)
+		redirect_to board_path(board)
 	end
 
 	def destroy
 		board= Board.find(params[:board_id])
 		bookmark = board.bookmarks.find_by!(user_id: current_user.id)
 		bookmark.destroy
-		redirect_to boards_path(board)
+		redirect_to board_path(board)
 	end
 end
