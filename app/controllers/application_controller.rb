e3adbe294cba0ceb6d19c59c031d2e6dee67ffe4
KEI <>
2020-11-17 14:54:13 +0900

    ユーザーログイン後画面の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-	def after_sign_in_path_for(resource)
+	def after_sign_up_path_for(resource)
+    edit_profile_path
+  end
+
+  def after_sign_in_path_for(resource)
     boards_path
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_060237) do
+ActiveRecord::Schema.define(version: 2020_11_16_094818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_060237) do
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "nickname"
+    t.string "nickname", default: "Noname"
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
