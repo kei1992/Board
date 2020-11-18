b93d8b25cc6045e83c09911fc91c243a3bd4037b
KEI <>
2020-11-18 14:51:20 +0900

    Taskテーブルからdeadlineを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_050124) do
+ActiveRecord::Schema.define(version: 2020_11_18_054433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,7 +177,6 @@ ActiveRecord::Schema.define(version: 2020_11_18_050124) do
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name", null: false
-    t.date "deadline"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "board_id"
