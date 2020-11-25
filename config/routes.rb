e89b3e714eb96942413d6076d431077e5bd31485
KEI <>
2020-11-26 01:21:30 +0900

    エラーメッセージ日本語化＆レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
+  devise_for :users, controllers: {    omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
